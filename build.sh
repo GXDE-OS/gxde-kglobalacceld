017b0e58ddfee2064039c3ca6022291b282542c5
shenmo <jifengshenmo@outlook.com>
2025-05-06 12:17:30 +0800

    update: no need k9 anymore

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 pushd $(dirname $0)
 rm -vfr pkg
 mkdir -p pkg/usr/share/gxde-k9/edging
-cat > pkg/usr/share/gxde-k9/edging/gxde-kglobalacceld.edging << EOF
+cat > pkg/usr/share/gxde-k9/edging/gxde-kglobalacceld.edging.bak << EOF
 # 目标程序路径（需要运行的可执行文件或命令）
 TARGET_PROCESS="/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libexec/kglobalacceld"
 DONT_RUN_IF_SYSTEMD_EXIST=""
 
 EOF
-
+# We don't need k9 to activate anymore
 popd
